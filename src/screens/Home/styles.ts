0c61754ca98407b4b603ddf4c07a599b9150c989
Matheus Oliveira <mho.oliveira01@gmail.com>
2023-07-26 20:44:17 -0300

    TextInput

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -15,5 +15,13 @@ export const styles = StyleSheet.create({
   eventDate: {
     color: '#6B6B6B',
     fontSize: 16
+  },
+  input: {
+    backgroundColor: '#1F1E25',
+    height: 56,
+    borderRadius: 5,
+    color: '#FDFCFE',
+    padding: 16,
+    fontSize: 16
   }
 });

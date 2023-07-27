2e97364191e9bdbc4698fc521a40896b31313feb
Matheus Oliveira <matheus.oliveira@ivoryit.com.br>
2023-07-27 20:24:12 -0300

    Adicionando botao

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,11 @@
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
 
 export function Home() {
+  const handleParticipantAdd = () => {
+    console.log('Você clicou no botão de adicionar.')
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
@@ -9,10 +13,14 @@ export function Home() {
       <Text style={styles.eventDate}>Sexta, 23 de junho de 2023</Text>
 
       <TextInput
-        style={styles.input}
-        placeholder='Nome do participante'
-        placeholderTextColor="#6B6B6B"
+      style={styles.input}
+      placeholder='Nome do participante'
+      placeholderTextColor="#6B6B6B"
       />
+
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 }
